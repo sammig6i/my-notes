ad1e8ebc641377ace0a581e84f35ae5429f65636
SavviSam <swghazzawi@gmail.com>
2024-03-05 14:20:09 -0700

    Quartz sync: Mar 5, 2024, 2:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "sammi24.github.io/my-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
